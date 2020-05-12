ca91f5b9eef9760e132e7ba767f92484ad158791
okaheel <omar.kaheel@colorado.edu>
2020-05-12 02:09:28 -0600

    error in python script corrected

--- git-actions.py
+++ git-actions.py
@@ -8,5 +8,5 @@ unrate = pd.read_csv('unrate.csv')
 first_twelve = unrate[0:12]
 #graph date on x and value on y
 figure = plt.plot(first_twelve['DATE'], first_twelve['VALUE'])
-figure.savefig('plot.png')
+plt.savefig('plot.png')
 plt.close(figure)
